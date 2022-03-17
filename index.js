bada3446f3e86816aa3fdbe44edf725827852064
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2022-03-17 08:05:44 -0300

    testando CLI

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send(`<h1>Executando na porta ${port}</h1>`);
 });
 
